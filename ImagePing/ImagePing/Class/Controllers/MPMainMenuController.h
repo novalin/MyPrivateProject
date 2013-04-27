0152a111b6fc35bfd26b0017a9f1c6e5b0ef8b65
novalin <linzhu0831@gmail.com>
2013-04-27 10:47:58 +0800

    update

---
+++ ImagePing/ImagePing/Class/Controllers/MPMainMenuController.h
@@ -0,0 +1,13 @@
+//
+//  MPMainMenuController.h
+//  ImagePing
+//
+//  Created by linzhu on 13-4-25.
+//  Copyright (c) 2013年 linzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPMainMenuController : UIViewController
+
+@end
---
+++ ImagePing/ImagePing/Class/Controllers/MPMainMenuController.m
@@ -0,0 +1,38 @@
+//
+//  MPMainMenuController.m
+//  ImagePing
+//
+//  Created by linzhu on 13-4-25.
+//  Copyright (c) 2013年 linzhu. All rights reserved.
+//
+
+#import "MPMainMenuController.h"
+
+@interface MPMainMenuController ()
+
+@end
+
+@implementation MPMainMenuController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
